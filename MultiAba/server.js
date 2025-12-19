5712f4a7b6997696ad23672bbafa46f631a3e297
Matramos08 <ramosmatheus92@gmail.com>
2025-12-18 21:47:32 -0300

    Refactor server.js to include path module

--- MultiAba/server.js
+++ MultiAba/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs'); 
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', { 
@@ -25,4 +25,5 @@ app.get('/contato', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+
 });
