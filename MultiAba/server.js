117e0cb87babbea416e24f240dcc7770dd1fe9d1
Matramos08 <ramosmatheus92@gmail.com>
2025-12-18 21:37:04 -0300

    Add files via upload

---
+++ MultiAba/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs'); 
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index', { 
+        pageTitle: 'Página Inicial',
+    });
+});
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre', { 
+        pageTitle: 'Sobre a Aplicação',
+    });
+});
+
+app.get('/contato', (req, res) => {
+    res.render('contato', { 
+        pageTitle: 'Entre em Contato',
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
