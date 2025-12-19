7b4c5e19a41c8991902a22e53ba89f7ed1f9a84c
Matramos08 <ramosmatheus92@gmail.com>
2025-12-18 21:49:11 -0300

    Define PORT variable for server configuration
    Added PORT variable to configure server port.

--- MultiAba/server.js
+++ MultiAba/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000; 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,6 +25,5 @@ app.get('/contato', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
